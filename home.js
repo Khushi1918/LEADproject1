433016f99b13930c883a51e4071657bbf16d4bf1
Khushi1918 <btech10369.21@bitmesra.ac.in>
2022-11-06 00:33:42 +0530

    second commit

---
+++ home.js
@@ -0,0 +1,16 @@
+document.querySelector("#nav-home").addEventListener("click",() =>{
+    document.querySelector("#home").scrollIntoView({behavior: "smooth"});
+})
+
+document.querySelector("#nav-about").addEventListener("click",() =>{
+    document.querySelector("#about").scrollIntoView({behavior: "smooth"});
+})
+
+document.querySelector("#nav-skills").addEventListener("click",() =>{
+    document.querySelector("#skills").scrollIntoView({behavior: "smooth"});
+})
+
+document.querySelector("#nav-projects").addEventListener("click",() =>{
+    document.querySelector("#projects").scrollIntoView({behavior: "smooth"});
+})
+
